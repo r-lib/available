ef8e74c428ffe5aa22e039e5cfcecfff6720a8ec
Jim Hester <james.f.hester@gmail.com>
2017-04-28 11:02:14 -0400

    Re-document

--- R/available.R
+++ R/available.R
@@ -2,6 +2,7 @@
 #'
 #' @param name Name of package to search
 #' @param ... Additional arguments passed to [utils::available.packages()].
+#' @export
 available <- function(name, ...) {
   cran_names <- rownames(available_packages(...))
 
