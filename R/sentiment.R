6bba9dec7cf950921e1d2daf5ee59e7c6bef5315
Molly Lewis <mollyllewis@gmail.com>
2017-05-25 13:42:28 -0700

    restructure get_sentiment

--- R/sentiment.R
+++ R/sentiment.R
@@ -1,9 +1,12 @@
+sents = tidytext::get_sentiments("bing") 
+
 get_sentiment <- function(name) {
-    sents = tidytext::get_sentiments("bing") 
     name_sentiment = sents[sents$word == name, "sentiment"]
 
     if (nrow(name_sentiment) > 0) {
-      message(sprintf("`%s` has a  `%s` sentiment", name, unlist(name_sentiment)))
+      return(name_sentiment[[1]])
+    } else {
+      return(NULL)
     }
 }
     
