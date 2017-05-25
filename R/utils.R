7c40bd8427ba234509ee32bfbefc9f53eb399633
Jim Hester <james.f.hester@gmail.com>
2017-05-25 14:46:36 -0700

    Print method for package name validity

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,11 @@
 "%:::%" <- function(p, f) {
   get(f, envir = asNamespace(p))
 }
+
+yes_no <- function(x) {
+  if (isTRUE(x)) {
+    crayon::green("Yes")
+  } else {
+    crayon::red("No")
+  }
+}
--- R/valid_name.R
+++ R/valid_name.R
@@ -1,13 +1,22 @@
-# Algorithm used in R CMD check at
-# https://github.com/wch/r-source/blob/a3a73a730962fa214b4af0ded55b497fb5688b8b/src/library/tools/R/QC.R#L3214
+#' Is a package name valid
+#' @export
 valid_package_name <- function(name) {
+
+  # This is the algorithm used in R CMD check at
+  # https://github.com/wch/r-source/blob/a3a73a730962fa214b4af0ded55b497fb5688b8b/src/library/tools/R/QC.R#L3214
   valid <- TRUE
-# check for a package called 'R'
-  if (tolower(name) == "r") {
+
+  # check for a package called 'R'
+  res <- if (tolower(name) == "r") {
     FALSE
   } else {
     grepl(valid_package_name_regexp, name)
   }
+  structure(res, class = "available_valid_name")
+}
+
+print.available_valid_name <- function(x) {
+  cat(crayon::bold("Valid:"), yes_no(x[[1]]), "\n")
 }
 
 valid_package_name_regexp <-
