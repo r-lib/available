7bdaaab9dfa6d2f5928d01be91aafc204091f57b
carlganz <carlganz@gmail.com>
2017-05-25 13:38:38 -0700

    initial name distance

---
+++ R/dist.R
@@ -0,0 +1,14 @@
+#' Find five closest package names in terms of string distance
+#'
+#' @param name name of package
+#' @param pkgs packages to compare with
+#'
+pkg_name_dist <- function(name, pkgs) {
+  distances <- stringdist::stringdist(name, pkgs)
+
+  tibble::tibble(
+    pkgs = head(pkgs[order(distances)]),
+    distance = head(distances[order(distances)])
+  )
+
+}
