a8537cd8cf7cd95ee7d82f05ed12c0a87229ce03
Jim Hester <james.f.hester@gmail.com>
2017-05-25 15:07:58 -0700

    Print method for sentiment query

--- R/sentiment.R
+++ R/sentiment.R
@@ -1,18 +1,23 @@
 #' Get sentiment of name
 #'
 #' @param name Name of package to search
+sentiment <- function(name) {
+  sentiment_dictionary <- tidytext::get_sentiments("bing")
 
-sents = tidytext::get_sentiments("bing") 
-
-get_sentiment <- function(name) {
-    name_sentiment = sents[sents$word == name, "sentiment"]
-
-    if (nrow(name_sentiment) > 0) {
-      name_sentiment[[1]]
-    } else {
-      NULL
-    }
+  res <- sentiment_dictionary[sentiment_dictionary$word == name, ][["sentiment"]]
+  if (length(res) == 0) {
+    res <- NA
+  }
+  structure(res == "positive", class = "available_sentiment")
 }
-    
-    
 
+print.available_sentiment <- function(x) {
+  cat(crayon::bold("Sentiment:"),
+    if (is.na(x[[1]])) {
+      "???"
+    } else if (isTRUE(x[[1]])) {
+      crayon::green("\u2795")
+    } else{
+      crayon::red("\u2796")
+    }, "\n")
+}
