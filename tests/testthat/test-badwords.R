aac7735a20106dbca24302b54746f2dc6ed6a5d0
carlganz <carlganz@gmail.com>
2017-05-26 12:14:37 -0700

    fix test for new bad words func

--- tests/testthat/test-badwords.R
+++ tests/testthat/test-badwords.R
@@ -1,9 +1,9 @@
 context("Check package name for bad words")
 
 test_that("Catches bad word", {
-  expect_error(get_bad_words("hell"))
+  expect_true(length(get_bad_words("hell"))>0)
 })
 
 test_that("Passes safe word", {
-  expect_true(get_bad_words("happy"))
+  expect_true(length(get_bad_words("happy"))==0)
 })
