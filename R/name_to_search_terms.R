8e3b447f08a71e4961f2e7a69b23d0b7ef27ab1f
carlganz <carlganz@gmail.com>
2017-05-26 16:46:23 -0700

    don't split acronyms

--- R/name_to_search_terms.R
+++ R/name_to_search_terms.R
@@ -1,8 +1,11 @@
 # Function that takes a name and generates search terms based on it
 name_to_search_terms <- function(name){
+  if (name == toupper(name)) {
+    return(name)
+  }
   # generate test strings by splitting on & removing periods & digits
   output <- strsplit(x = name, split = "[[:digit:][:punct:]]")
-  
+
   # generate test strings by splitting on and removing uppercase letters
   removedUppers <- strsplit(x = output[[1]], split = "[[:upper:]]")
   # split on but do not remove uppercase characters
@@ -10,13 +13,13 @@ name_to_search_terms <- function(name){
   uppersFirst <- strsplit(gsub('([[:upper:]])', ' \\1', output[[1]]), split = " ")
   # assume uppercase last letter in word (it happens!)
   uppersLast <- strsplit(gsub('([[:upper:]])', '\\1 ', output[[1]]), split = " ")
-  
+
   # combine list of search terms generated based on different assumptions about use of capitals
   search_terms <- unique(unlist(c(uppersFirst, uppersLast, removedUppers)))
-  
+
   # generate test strings by splitting on/removing r
   #searchTermsWithR <- unique(c(searchTerms, unlist(strsplit(x = searchTerms, split = "R"))))
-  
+
   # remove elements that are just "r", "R" or empty & add name
   search_terms <- c(setdiff(search_terms, c("r","R","", name)), name)
 
