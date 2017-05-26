89118732a249d4c128abd8f441573248e63969a3
Gábor Csárdi <csardi.gabor@gmail.com>
2017-05-26 11:31:15 -0700

    Print Urban Dictionary result
    First definition and a link, for now.

--- R/get_urban_data.R
+++ R/get_urban_data.R
@@ -2,8 +2,21 @@
 #'
 #' @param name Name of package to search
 get_urban_data<- function(name) {
-  defs <- purrr::safely(head)(udapi::get_term(name)$definition)
-  tags <- purrr::safely(head)(udapi::get_tags(name)$tags)
+  term <- tryCatch(udapi::get_term(name), error = function(e) e)
+  tags <- tryCatch(udapi::get_tags(name)$tags, error = function(e) e)
+  structure(list(as.data.frame(term), tags), class = "available_urban")
+}
 
-  structure(list(defs$result, tags$result), class = "urban")
+print.available_urban <- function(x, ...) {
+  cat(crayon::bold("Urban Dictionary:\n"))
+
+  ## Format first definition
+  first <- fix_windows_nl(x[[1]]$definition[[1]])
+  cat(strwrap(first, indent = 2, exdent = 2), sep = "\n")
+
+  ## Link
+  cat("\n  ", crayon::blue(x[[1]]$permalink[1]), sep = "")
+  cat("\n")
+
+  invisible(x)
 }
