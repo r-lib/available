bc4117f03ac9ec17eb8e4d02e666981f9a19e5c6
Jim Hester <james.f.hester@gmail.com>
2017-05-26 10:21:44 -0700

    Open wikipedia and wiktionary urls

--- R/open_urls.R
+++ R/open_urls.R
@@ -2,15 +2,30 @@
 #'
 #' @param name Name of package to search
 
-get_sentiment <- function(name) {
-  structure(c(browseURL(paste0("https://en.wikipedia.org/wiki/", name)),
-              browseURL(paste0("http://www.abbreviations.com/", name))),
-            class = "urls")
-  
+get_wikipidia <- function(name) {
+  # TODO: handle case when we can't open browser
+  url <- paste0("https://en.wikipedia.org/wiki/", name)
+
+  structure(url, class = "available_wikipedia")
 }
 
+print.available_wikipedia <- function(x) {
+  browseURL(x[[1]])
+  cat(crayon::bold("Wikipedia: "), x[[1L]], "\n", sep = "")
 
+  invisible(x)
+}
 
+get_wiktionary <- function(name) {
+  # TODO: handle case when we can't open browser
+  url <- paste0("https://en.wiktionary.org/wiki/", name)
 
+  structure(url, class = "available_wiktionary")
+}
 
+print.available_wiktionary <- function(x) {
+  browseURL(x[[1]])
+  cat(crayon::bold("Wikipedia: "), x[[1L]], "\n", sep = "")
 
+  invisible(x)
+}
