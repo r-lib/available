c973c791c94eb062910a0aaf2bfdf1dc14bcb490
Jim Hester <james.f.hester@gmail.com>
2017-05-26 15:11:52 -0700

    Query abbreviations by default

--- R/available.R
+++ R/available.R
@@ -16,6 +16,7 @@ available <- function(name, ...) {
     available_on_bioc(name, ...),
     available_on_github(name),
     get_bad_words(name_to_search_terms(name)),
+    get_abbreviation(name),
     get_wikipidia(name),
     get_wiktionary(name),
     get_urban_data(name),
