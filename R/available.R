4ed47aef887c66fdd7117485b8f424268dd25e77
Jim Hester <james.f.hester@gmail.com>
2017-05-26 11:23:43 -0700

    Move order back

--- R/available.R
+++ R/available.R
@@ -12,11 +12,11 @@
 available <- function(name, ...) {
   structure(list(
     valid_package_name(name),
-    get_wikipidia(name),
-    get_wiktionary(name),
     available_on_cran(name, ...),
     available_on_bioc(name, ...),
     available_on_github(name),
+    get_wikipidia(name),
+    get_wiktionary(name),
     get_urban_data(name),
     sentiment(name)),
     class = "available_query")
